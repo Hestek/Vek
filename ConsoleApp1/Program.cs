2fb854bf9f84e19547c77a6171640b6af6911275
Patrik Sigl <Sigl.Patrik@stud.vassboskovice.cz>
2021-10-26 11:36:03 +0200

    JE TO ŠPATNĚ, UPRAVIT KONEC

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,6 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            double vek;
             double rok;
             double vypocet;
 
@@ -20,11 +19,26 @@ namespace ConsoleApp1
             Console.WriteLine();
             Console.ReadLine();
 
-            vypocet = ( - rok);
+            vypocet = (DateTime.Now.Year - rok);
             if (vypocet > 100) 
             Console.WriteLine("Neplatny rok narození");
-            if (vypocet < 100) 
-            Console.WriteLine($"Je vam { vypocet } let");
+            if (rok == 2021)
+                Console.WriteLine("Neplatny rok narození");
+            if (vypocet < 6) 
+            Console.WriteLine($"Je vam { vypocet } let, jste stále jen dítě");
+            if (vypocet  > 15)
+            Console.WriteLine($"Je vam { vypocet } let, jste už teenager");
+            if (vypocet > 18)
+                Console.WriteLine($"Je vam { vypocet } let, jste už dospělý");
+            if (vypocet > 30)
+                Console.WriteLine($"Je vam { vypocet } let, Nejelpší léta už jsou z vámi");
+            if (vypocet > 40)
+                Console.WriteLine($"Je vam { vypocet } let, stále nejste moc starý");
+            if (vypocet > 50)
+                Console.WriteLine($"Je vam { vypocet } let, pomaličku už se chystejte do důchodu");
+            if (vypocet > 65)
+                Console.WriteLine($"Je vam { vypocet } let, už jste v důchodu");
+
 
         }
     }
