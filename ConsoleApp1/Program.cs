3babb9525c098b85bb16ab06152eadfba26856a7
Patrik Sigl <Sigl.Patrik@stud.vassboskovice.cz>
2021-10-26 11:15:11 +0200

    funguje

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,11 +5,28 @@ namespace ConsoleApp1
     class Program
     {
         static void Main(string[] args)
-        double narozeni;
-        double vek;
-
         {
-            Console.WriteLine("Hello World!");
+            double vek;
+            double rok;
+            double vypocet;
+
+            Console.CursorVisible = false;
+            Console.SetCursorPosition(10, 10);
+            Console.WriteLine();
+            Console.SetCursorPosition(10, 11);
+            Console.WriteLine("Zadejte svůj rok narození");
+            double.TryParse(Console.ReadLine(), out rok);
+            Console.SetCursorPosition(10, 12);
+            Console.WriteLine();
+            Console.ReadLine();
+            
+
+            vypocet = (2021 - rok);
+            if (vypocet > 100) 
+            Console.WriteLine("Neplatny rok narození");
+            if (vypocet < 100) 
+            Console.WriteLine($"Je vam { vypocet } let");
+
         }
     }
 }
